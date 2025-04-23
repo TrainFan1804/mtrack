b02d8aa9bd4df4278bf318107c5701af77fb1c5c
o.le <oleschulz123@gmail.com>
2025-04-23 19:46:04 +0200

    Adding a TopLevel window when adding a new media

--- gui/MTrack.py
+++ gui/MTrack.py
@@ -6,17 +6,27 @@ import msgsender
 import responsecodes as rsp
 
 
+def place_window_mid(window, width, height):
+    # get screen width and height# 
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    
+    # # calculate position x and y coordinates
+    x = (screen_width / 2) - (width / 2)
+    y = (screen_height / 2) - (height / 2)
+    window.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+
 class MTrack(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        self._place_window_mid(500, 400)
+        place_window_mid(self, 500, 400)
 
         self.test_connection_label = tk.Label(text="Nothing right now", background="red")
         self.test_connection_label.pack()
 
         tk.Label(text="mTrack",
-                    width=15,
-                    height=5).pack()
+                    height=3).pack()
 
         self.tree = ttk.Treeview(selectmode='browse', show='headings', columns=('name', 'rating'))
         self.tree.heading('name', text='name')
@@ -35,21 +45,16 @@ class MTrack(tk.Tk):
                         command=self.destroy).pack(side=tk.LEFT)
         btn_frame.pack()
 
-    
-    def _place_window_mid(self, width, height):
-        # get screen width and height# 
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-        
-        # # calculate position x and y coordinates
-        x = (screen_width / 2) - (width / 2)
-        y = (screen_height / 2) - (height / 2)
-        self.geometry('%dx%d+%d+%d' % (width, height, x, y))
-
 
     def _add_item(self):
-        # TODO open seperate window and extract data from there?
         msgsender.send_request_no_data(rsp.ADD_RESPONSE)
+        AddTopLevel(self, self._receive_data_callback)
+
+
+    def _receive_data_callback(self, values):
+        for v in values:
+            # TODO add sending of data
+            print(v, file=sys.stderr)
 
 
     def _rm_item(self):
@@ -63,3 +68,40 @@ class MTrack(tk.Tk):
         if rsp_code == rsp.TRN_END:
             selectd_item = self.tree.selection()[0]
             self.tree.delete(selectd_item)
+
+
+class AddTopLevel(tk.Toplevel):
+    def __init__(self, parent, callback):
+        tk.Toplevel.__init__(self, parent)
+        place_window_mid(self, 400, 300)
+
+        self.callback = callback
+
+        tk.Label(self, text="Add a new media", height=3).pack()
+        self._build_entries()
+        btn_frame = tk.Frame(self)
+        tk.Button(btn_frame, text="add", command=self._send_data).pack(side=tk.LEFT)
+        tk.Button(btn_frame, text="close", command=self.destroy).pack(side=tk.LEFT)
+        btn_frame.pack()
+
+
+    def _build_entries(self):
+        """
+            NOTE: When adding a new attribute to the database, a new label and 
+            frame for the toplevel window need to be added.
+        """
+        self.entry_frame = tk.Frame(self)
+        name_list = ["name", "rating"]
+        for _row in range(len(name_list)):
+            tk.Label(self.entry_frame, text=name_list[_row]).grid(row=_row, column=0)
+            tk.Entry(self.entry_frame).grid(row=_row, column=1)
+        self.entry_frame.pack()
+
+
+    def _send_data(self):
+        data = []
+        for child in self.entry_frame.winfo_children():
+            if (isinstance(child, tk.Entry)):
+                data.append(child.get().strip())
+        # TODO add a check for data completion
+        self.callback(data)
